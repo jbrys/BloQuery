6e9acd1875693c54af5ad9bcf8f59e48bb7673a9
Jeff Brys <jeffreybrys@gmail.com>
2016-01-13 18:42:30 -0800

    updated drawer menu to NavigationView

--- app/src/main/java/com/jbrys/android/bloquery/ui/activity/BloQueryActivity.java
+++ app/src/main/java/com/jbrys/android/bloquery/ui/activity/BloQueryActivity.java
@@ -2,11 +2,14 @@ package com.jbrys.android.bloquery.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jbrys.android.bloquery.BloQueryApplication;
@@ -19,13 +22,16 @@ import com.parse.ParseUser;
 /**
  * Created by jeffbrys on 11/23/15.
  */
-public class BloQueryActivity extends AppCompatActivity implements
-        Button.OnClickListener,
-        QuestionsFragment.Listener{
+public class BloQueryActivity extends AppCompatActivity implements QuestionsFragment.Listener{
+
+
     private final String TAG = getClass().getSimpleName();
 
     private ParseUser currentUser;
 
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ImageView mDrawerHeaderBg;
     private TextView userNameTextView;
     private MenuItem answerItem;
 
@@ -35,8 +41,36 @@ public class BloQueryActivity extends AppCompatActivity implements
 
         setContentView(R.layout.bloquery_activity);
 
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if (item.getItemId() == R.id.logout) {
+                    ParseUser.logOutInBackground();
+                    userNameTextView.setText(null);
+                    startActivity(new Intent(BloQueryActivity.this, LoginActivity.class));
+                    onDrawerItemClicked(item);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (toolbar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+        }
 
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.bloquery_list_layout, new QuestionsFragment())
@@ -44,14 +78,18 @@ public class BloQueryActivity extends AppCompatActivity implements
 
         currentUser = BloQueryApplication.getCurrentUser();
 
-        userNameTextView = (TextView) findViewById(R.id.bloquery_tv_userName);
+        View headerView = mNavigationView.getHeaderView(0);
+        userNameTextView = (TextView) headerView.findViewById(R.id.bloquery_tv_userName);
+
+        ImageView headerBg = (ImageView) headerView.findViewById(R.id.header_bg);
+        headerBg.setImageResource(R.drawable.header_bg);
+        headerBg.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
 
         if (currentUser != null) {
             userNameTextView.setText(currentUser.getUsername());
         }
 
-        Button logout = (Button) findViewById(R.id.bloquery_btn_logout);
-        logout.setOnClickListener(this);
 
     }
 
@@ -84,21 +122,10 @@ public class BloQueryActivity extends AppCompatActivity implements
 
             getFragmentManager().popBackStack();
         }
-    }
-
-
-
-    /*
-    * OnClickListeners
-    */
-
-    @Override
-    public void onClick(View v) {
-        ParseUser.logOutInBackground();
-        userNameTextView.setText(null);
-        startActivity(new Intent(BloQueryActivity.this, LoginActivity.class));
 
     }
+
+
 
     @Override
     public void onItemAnswersClicked(QuestionsFragment questionsFragment, Question question) {
@@ -108,6 +135,10 @@ public class BloQueryActivity extends AppCompatActivity implements
                 .addToBackStack("ANSWER")
                 .commit();
 
+    }
+
+    void onDrawerItemClicked(MenuItem item){
+        mDrawerLayout.closeDrawer(GravityCompat.START);
     }
 
 
--- app/src/main/java/com/jbrys/android/bloquery/ui/fragment/UserFragment.java
+++ app/src/main/java/com/jbrys/android/bloquery/ui/fragment/UserFragment.java
@@ -16,6 +16,6 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_fragment, container, false);
+        return inflater.inflate(R.layout.drawer_header, container, false);
     }
 }
